de76f8a33867ecbea94f17cf41cdaf074f11481b
RakshetaK <15raksheta.k@gmail.com>
2026-04-16 23:35:46 -0700

    mario controller sprite accessing completed, motion in the works w gravity

--- scripts/mario_controller.v
+++ scripts/mario_controller.v
@@ -1,87 +1,108 @@
 `timescale 1ns / 1ps
 
-module block_controller(
+module mario(
 	input clk, //this clock must be a slow enough clock to view the changing positions of the objects
-	input bright,
 	input rst,
-	input up, input down, input left, input right,
+	input btnU, btnL, btnR,
 	input [9:0] hCount, vCount,
+	output reg[9:0] mario_x, mario_y,
 	output reg [11:0] rgb,
-	output reg [11:0] background
+	output valid
    );
-	wire block_fill;
 	
+	//sprite size
+	localparam  width = 16;
+	localparam  height = 16;
+
+	//check if pixel is in bounds of mario
+	wire mario_bound;
+	assign mario_bound = (hCount >= mario_x && hCount < mario_x + width 
+						&& vCount >= mario_y && vCount < mario_y + height)
+	//sprite pixel
+	wire [3:0] sprite_x = hCount - mario_x;
+	wire [3:0] sprite_y = vCount - mario_y;
+
+	//instantiate rom
+	wire[11:0] sprite_color;
+
+	mario-sprite_rom u_rom(
+		.clk(clk),
+		.sprite_x(row),
+		.sprite_y(col),
+		.sprite_color(color_data)
+	);
+	
+	assign valid = in_sprite;
+	assign sprite_color = color_data;
+
+	//top-left of mario
+	if(btnR && ~btnL && hCount < 624)
+		mario_x <= mario_x + 2;
+	if(btnL && ~btnR && vCount > 0 )
+		mario_x <= mario_x - 2;
+	if(btnU)
+
+
+
 	//these two values dictate the center of the block, incrementing and decrementing them leads the block to move in certain directions
-	reg [9:0] xpos, ypos;
+	// reg [9:0] xpos, ypos;
 	
-	parameter RED   = 12'b1111_0000_0000;
+	// parameter RED   = 12'b1111_0000_0000;
 	
-	/*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
-	will output some data to every pixel and not just the images you are trying to display*/
-	always@ (*) begin
-    	if(~bright )	//force black if not inside the display area
-			rgb = 12'b0000_0000_0000;
-		else if (block_fill) 
-			rgb = RED; 
-		else	
-			rgb=background;
-	end
-		//the +-5 for the positions give the dimension of the block (i.e. it will be 10x10 pixels)
-	assign block_fill=vCount>=(ypos-5) && vCount<=(ypos+5) && hCount>=(xpos-5) && hCount<=(xpos+5);
+	// /*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
+	// will output some data to every pixel and not just the images you are trying to display*/
+	// always@ (*) begin
+    // 	if(~bright )	//force black if not inside the display area
+	// 		rgb = 12'b0000_0000_0000;
+	// 	else if (block_fill) 
+	// 		rgb = RED; 
+	// 	else	
+	// 		rgb=background;
+	// end
+	// 	//the +-5 for the positions give the dimension of the block (i.e. it will be 10x10 pixels)
+	// assign block_fill=vCount>=(ypos-5) && vCount<=(ypos+5) && hCount>=(xpos-5) && hCount<=(xpos+5);
 	
-	always@(posedge clk, posedge rst) 
-	begin
-		if(rst)
-		begin 
-			//rough values for center of screen
-			xpos<=450;
-			ypos<=250;
-		end
-		else if (clk) begin
+	// always@(posedge clk, posedge rst) 
+	// begin
+	// 	if(rst)
+	// 	begin 
+	// 		//rough values for center of screen
+	// 		xpos<=450;
+	// 		ypos<=250;
+	// 	end
+	// 	else if (clk) begin
 		
-		/* Note that the top left of the screen does NOT correlate to vCount=0 and hCount=0. The display_controller.v file has the 
-			synchronizing pulses for both the horizontal sync and the vertical sync begin at vcount=0 and hcount=0. Recall that after 
-			the length of the pulse, there is also a short period called the back porch before the display area begins. So effectively, 
-			the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
-			corresponds to ~(783,515).  
-		*/
-			if(right) begin
-				xpos<=xpos+2; //change the amount you increment to make the speed faster 
-				if(xpos==800) //these are rough values to attempt looping around, you can fine-tune them to make it more accurate- refer to the block comment above
-					xpos<=150;
-			end
-			else if(left) begin
-				xpos<=xpos-2;
-				if(xpos==150)
-					xpos<=800;
-			end
-			else if(up) begin
-				ypos<=ypos-2;
-				if(ypos==34)
-					ypos<=514;
-			end
-			else if(down) begin
-				ypos<=ypos+2;
-				if(ypos==514)
-					ypos<=34;
-			end
-		end
-	end
+	// 	/* Note that the top left of the screen does NOT correlate to vCount=0 and hCount=0. The display_controller.v file has the 
+	// 		synchronizing pulses for both the horizontal sync and the vertical sync begin at vcount=0 and hcount=0. Recall that after 
+	// 		the length of the pulse, there is also a short period called the back porch before the display area begins. So effectively, 
+	// 		the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
+	// 		corresponds to ~(783,515).  
+	// 	*/
+	// 		if(right) begin
+	// 			xpos<=xpos+2; //change the amount you increment to make the speed faster 
+	// 			if(xpos==800) //these are rough values to attempt looping around, you can fine-tune them to make it more accurate- refer to the block comment above
+	// 				xpos<=150;
+	// 		end
+	// 		else if(left) begin
+	// 			xpos<=xpos-2;
+	// 			if(xpos==150)
+	// 				xpos<=800;
+	// 		end
+	// 		else if(up) begin
+	// 			ypos<=ypos-2;
+	// 			if(ypos==34)
+	// 				ypos<=514;
+	// 		end
+	// 		else if(down) begin
+	// 			ypos<=ypos+2;
+	// 			if(ypos==514)
+	// 				ypos<=34;
+	// 		end
+	// 	end
+	// end
 	
 	//the background color reflects the most recent button press
-	always@(posedge clk, posedge rst) begin
-		if(rst)
-			background <= 12'b1111_1111_1111;
-		else 
-			if(right)
-				background <= 12'b1111_1111_0000;
-			else if(left)
-				background <= 12'b0000_1111_1111;
-			else if(down)
-				background <= 12'b0000_1111_0000;
-			else if(up)
-				background <= 12'b0000_0000_1111;
-	end
+	
 
 	
 	
